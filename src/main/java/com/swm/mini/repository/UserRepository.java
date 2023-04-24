49eef1f83ecbbbe919c40498907ae69259eee58b
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-04-24 14:53:10 +0900

    feat: 레포지토리 잘못 만들어서 삭제했음

--- src/main/java/com/swm/mini/repository/UserRepository.java
+++ src/main/java/com/swm/mini/repository/UserRepository.java
@@ -3,12 +3,5 @@ package com.swm.mini.repository;
 import com.swm.mini.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    long deleteById(String userId);
-
-    boolean existsById(String userId);
-
-    Optional<User> findById(String userId);
+public interface UserRepository extends JpaRepository<User, String> {
 }
