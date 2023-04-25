98ac474c6155e1f9c34ff226b0d540853826cdc5
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-04-25 17:50:34 +0900

    feat: 에러코드에서 필요없는 속성 삭제

--- src/main/java/com/swm/mini/exception/MiniErrorCode.java
+++ src/main/java/com/swm/mini/exception/MiniErrorCode.java
@@ -6,22 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MiniErrorCode {
-    ALREADY_REGISTERED(500, "CREATE-USER-ERR-500", "is already registered."),
-    NO_USER(500, "USER-ERR-500", "There is no corresponding USER."),
-    NO_EVENT(500, "EVENT-ERR-500", "There is no corresponding EVENT."),
-    LIMIT_OVER(500, "EVENT-ERR-500", "cannot join this event."),
-    ALREADY_JOINED(500, "JOIN-EVENT-ERR-500", "You already joined this Event."),
-
-    WRONG_PASSWORD(500, "LOGIN-ERR-500", "Invalid password."),
-    WRONG_EMAIL(500, "LOGIN-ERR-500", "There is no such email information."),
-    NO_CATEGORY(500, "CATEGORY-ERR-500", "There is no corresponding CATEGORY."),
-    NO_QUEST(500, "QUEST-ERR-500", "There is no corresponding QUEST."),
-    NO_MEM_DO(500, "MEM_DO-ERR-500", "There is no quest that user challenging."),
-    NO_BADGE(500, "BADGE-SUPERUSER-ERROR", "There is no corresponding BADGE."),
-
+    ALREADY_REGISTERED("is already registered."),
+    NO_USER("There is no corresponding USER."),
+    NO_EVENT("There is no corresponding EVENT."),
+    LIMIT_OVER("cannot join this event."),
+    ALREADY_JOINED("You already joined this Event."),
+    WRONG_PASSWORD("Invalid password."),
     ;
 
-    private int status;
-    private String errorCode;
     private String message;
 }
